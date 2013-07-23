ea9868058d0ce735f2d20193d8a98a35c06648ca
charles <platypode@gmail.com>
2013-07-23 18:11:17 -0400

    fixed

--- scheduler.py
+++ scheduler.py
@@ -1,33 +1,56 @@
+class Node:
+    
+    def __init__(self, **kwargs):
+        self.id, self.name = kwargs['id'], kwargs['name']
+        self.pv = kwargs['pv']
+        self.dependency_names = kwargs['dependencies']
+        self.resource_reqs = kwargs['resource_reqs']
+        self.dependencies = kwargs['dependencies']
+
+
 from random import randint, random
 from math import exp, log
 import numpy as np
 from pprint import pprint
 import matplotlib.pyplot as plt
 
-class Project:
-    
-    def __init__(self, **kwargs):
-        self.id, self.name = kwargs['id'], kwargs['name']
-        self.pv = kwargs['pv']
-        self.dependencies = kwargs['dependencies']
-        self.concurrencies = kwargs['concurrencies']
-
-
 class Scheduler:
     
-    def __init__(self, projects, T0=30, c=30, u=10, r=1.5):
+    def __init__(self, projects, resource_constraints, T0=40, c=30, u=10, r=2.0):
         """
         Initialize constants, node values, activations, weights
         """
-        self.projects = projects
+        self.nodes = enumerate_projects(projects)
+        self.resource_constraints = resource_constraints
         self.T0 = float(T0)
         self.N = len(projects)
-        self.c = c
-        self.u = u
-        self.r = r
+        self.c = c  # constraint violation penalty
+        self.u = u  # utilization penalty
+        self.r = r  # 1 + discount rate
         self.init_nodes()
         self.init_weights()
         self.energy_trail = []
+        
+    def enumerate_projects(projects):
+        N = sum([p.duration for p in project])
+        projects_dict = {p.name : p for p in projects}
+        project_names = sorted(project_dict.keys())
+        nodes = []
+        i = 0
+        name2id = {}; id2node = [0 for _ in range(N)]
+    
+    for name in project_names:
+        p = project_dict[name]
+        node = Node(p, dependencies=np.zeros(N))
+        for j in range(p.duration):
+            id2proj.append(node)
+            if j > 0: node.dependencies[j-1] = 1
+        name2id[name] = (i, i+p.duration - 1)
+        i += p.duration
+        
+    for i, p in enumerate(id2proj):
+        for name in p.dependency_names:
+            p.dependencies[name2id[name][1]] = 1
         
     def temp(self, k):
         """
@@ -43,12 +66,16 @@ class Scheduler:
         self.v = []
         for p in self.projects:
             self.v.append([self.c*sum(p.dependencies) - p.pv*pow(self.r, -j) for j in range(self.N)])
-            
+    
+    """
     def concurrent(self, i, j):
-        """
-        Return True iff i, j are concurrent
-        """
+        "Return True iff i, j are concurrent"
         return bool(self.projects[i].concurrencies[j])
+    """
+    
+    def concurrent(self, i, j):
+        return all(sum([np.array(self.projects[k].resource_reqs) for k in [i, j]]) \
+            <= np.array(self.resource_constraints))
     
     def dependent(self, i, j):
         """
@@ -60,11 +87,16 @@ class Scheduler:
         """
         Return weight from node (m,n) to (i,j)
         """
+        
         if i==m and j==n: return 0
+        """
         if j == n:
+            
             return self.c * (1 - self.concurrent(i, m)) - self.c * self.dependent(i, m)
         if i == m:
-            return self.c
+            return self.c"""
+        if j==n and self.concurrent(i, m): return 0
+        if i==m or j==n: return self.c
         if j > n:
             return -1 * (self.c * self.dependent(i, m))
         return 0
@@ -92,8 +124,13 @@ class Scheduler:
         Return current energy
         """
         e = self.u*max(0.0, float(self.N - self.a.sum()))
+        
         for i in range(self.N):
+            used = sum([np.array(self.projects[j].resource_reqs) * self.a[i][j] for j in range(self.N)])
+            e += self.u * abs(self.resource_constraints - used).sum()
+            
             for j in range(self.N):
+                e += self.a[i][j]
                 e += self.a[i][j]*(self.v[i][j]+(self.a*self.weights[i][j]).sum())
         return e
     
@@ -165,3 +202,6 @@ class Scheduler:
         """
         plt.plot(self.energy_trail)
         plt.show()
+        
+    def show_soln(self):
+        
